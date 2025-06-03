74998b34e9e4daeceb5c41172b116a546d2a292b
Jkl88 <35424901+Jkl88@users.noreply.github.com>
2025-06-03 12:13:54 +0300

    Добавьте файлы проекта.

---
+++ src/motorControl.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		motorControl.cpp
+ Created:	03.06.2025 12:12:46
+ Author:	Oleg
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "motorControl.h"
+
+
---
+++ src/motorControl.h
@@ -0,0 +1,19 @@
+/*
+ Name:		motorControl.h
+ Created:	03.06.2025 12:12:46
+ Author:	Oleg
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _motorControl_h
+#define _motorControl_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
